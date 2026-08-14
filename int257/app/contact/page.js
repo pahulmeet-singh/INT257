d91b9ac482d1991b670745837c5325691f32d6b7
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-08-14 15:48:11 +0530

    creating custom routes and fragment wrapping

---
+++ int257/app/contact/page.js
@@ -0,0 +1,11 @@
+export default function Contact(){
+    return (
+        // <h1> Contact Page</h1>
+        // <p>This is the contact page.</p>
+        // to solve this error, you can use a fragment to wrap the elements
+        <>
+            <h1> Contact Page</h1>
+            <p>This is the contact page.</p>
+        </>
+    )
+}
---
+++ int257/app/teams/careers/page.js
@@ -0,0 +1,7 @@
+export default function Career(){
+    return(
+        <>
+        <h1> This is my careers page</h1>
+        </>
+    )
+}
