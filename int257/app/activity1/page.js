c2f86cdb332a3c88bc132911d4c812b835b45b26
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-04 15:37:57 +0530

    fetching user data

--- int257/app/activity1/page.js
+++ int257/app/activity1/page.js
@@ -1,33 +1,20 @@
+import { cache } from "react"
+
 // activity: fetch user's data json using fetch api and client side rendering in next js, and display the data.
-export default function User({params}){
-    const {id}=params;
-    const [userData, setUserData] = React.useState(null);
-
-    React.useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-                const data = await response.json();
-                setUserData(data);
-            } catch (error) {
-                console.error("Error fetching user data:", error);
-            }
-        };
-
-        fetchUserData();
-    }, [id]);
-
-    if (!userData) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div>
-            <h1>User ID: {id}</h1>
-            <p>Name: {userData.name}</p>
-            <p>Email: {userData.email}</p>
-            <p>Phone: {userData.phone}</p>
-            <p>Website: {userData.website}</p>
-        </div>  
-     )
-    }
+export default async function Activity1(){
+    const response= await fetch(`https://jsonplaceholder.typicode.com/users/${id}`,
+        {cache: "no-store"})
+    const data=await response.json()
+    return(
+        <div> 
+        {data.map((item) => (
+            <ul key={item.id}>
+                <li>{item.id}</li>
+                <li><strong>Name: </strong>{item.name}</li>
+                <li><strong>Username: </strong>{item.username}</li>
+                <li><strong>Email: </strong>{item.email}</li>
+            </ul>
+        ))}
+        </div>
+    )
+}
