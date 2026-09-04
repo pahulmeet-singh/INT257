513ea04ac5b4610e3c98ac5684f401ba3323fa71
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-04 15:42:19 +0530

    using Client Side Rendering

--- int257/app/activity1/page.js
+++ int257/app/activity1/page.js
@@ -1,15 +1,36 @@
 // activity: fetch user's data json using fetch api and client side rendering in next js, and display the data.
+// export default async function Activity1(){
+//     const response= await fetch(`https://jsonplaceholder.typicode.com/users`,
+//         {cache: "no-store"})
+//     const data=await response.json()
+//     return(
+//         <div> 
+//         {data.map((item) => (
+//             <ul key={item.id}>
+//                 <li><strong>ID: </strong>{item.id}</li>
+//                 <li><strong>Name: </strong>{item.name}</li>
+//                 <li><strong>Username: </strong>{item.username}</li>
+//                 <li>------------------------------------------</li>
+//             </ul>
+//         ))}
+//         </div>
+//     )
+// }
+
+
+
+//CLIENT SIDE RENDERING
+"use client"
 export default async function Activity1(){
-    const response= await fetch(`https://jsonplaceholder.typicode.com/users`,
-        {cache: "no-store"})
+    const response= await fetch(`https://jsonplaceholder.typicode.com/users`)
     const data=await response.json()
     return(
         <div> 
         {data.map((item) => (
             <ul key={item.id}>
+                <li><strong>ID: </strong>{item.id}</li>
                 <li><strong>Name: </strong>{item.name}</li>
                 <li><strong>Username: </strong>{item.username}</li>
-                <li><strong>Email: </strong>{item.email}</li>
                 <li>------------------------------------------</li>
             </ul>
         ))}
