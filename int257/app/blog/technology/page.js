b7344f59b6bac4430a23b08ea92a636529ecb07a
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-08 16:25:58 +0530

    ca1 prep

---
+++ int257/app/blog/technology/page.js
@@ -0,0 +1,20 @@
+export const revalidate=60
+import React from 'react'
+
+export default async function Technology() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {cache: 'force-cache'})
+    const data = await response.json()
+    const posts=data.filter((item)=>item.userId===1)
+    return (
+        <div>           
+            {posts.map((post)=>(
+                <div>
+                    Id:<p>{post.id}</p>
+                    userId:<p>{post.userId}</p>
+                    title:<p>{post.title}</p>
+                    body:<p>{post.body}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
