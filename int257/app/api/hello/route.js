d8990c1d9d7af846cf393b57f710ff7bd0bf915e
Pahulmeet Singh <pahulmeetsingh96@gmail.com>
2026-09-15 15:47:25 +0530

    unit 3 started- APIs

---
+++ int257/app/api/hello/route.js
@@ -0,0 +1,14 @@
+export async function GET(){
+    function Response.json({
+        name:"Pahul",
+        id:1,
+        course:"B.Tech"
+    })
+}
+
+/*wht is api and what does it do?
+An API (Application Programming Interface) is a set of rules and protocols that allows different software applications to communicate with each other. It defines the methods and data formats that applications can use to request and exchange information.
+
+In the context of web development, ***an API allows developers to interact with a server or service to retrieve or send data.*** For example, in your code snippet, the GET function is part of an API endpoint that responds to HTTP GET requests. When a client (like a web browser or another application) sends a GET request to this endpoint, the server responds with a JSON object containing information about a person named "Pahul," including their ID and course.
+
+APIs are essential for building modern applications, as they enable integration between different services, facilitate data sharing, and allow developers to create more dynamic and interactive user experiences.
